b797ddbad2b813ed65a0fc9ace11dfb597e8f6ee
Fran <jopeta20000@gmail.com>
2024-02-06 09:41:19 +0100

    busqueda binaria implementación

--- algoritmos/busqueda.c
+++ algoritmos/busqueda.c
@@ -3,20 +3,19 @@
 int busquedaBinaria(int arr[], int length, int target){ //O(logN)
     int min = 0;
     int max = length;
-    int mitad = (min + length)/2;
-   
+
     do {
-        mitad = (min + length)/2;
+        int mitad = (max + min)/2; 
         int value = arr[mitad];
         
         if (value == target) {
             return mitad; 
-        }else if (value > mitad) {
-            min = mitad + 1; 
-        }else {
+        }else if (value > target) {
             max = mitad;
+        }else {
+            min = mitad + 1; 
         }
-    
+
     }while (min < max);
 
     return -1;
@@ -42,6 +41,7 @@ int main(void){
     int a[] = {7,3,5};
     int b[] = {0,1,2,3,4,5,6,7,8};
     printf("indice %d\n",busquedaLineal(a, 3, 5));
-    printf("el indice en la busqueda binaria es %d\n", busquedaBinaria(b, 9, 5));
+    printf("el indice en la busqueda binaria es %d\n",
+           busquedaBinaria(b, (sizeof(b)/sizeof(int)), 1));
     return 0;
 }
