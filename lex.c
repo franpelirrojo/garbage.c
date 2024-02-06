76901edb1948facc47434bd5ff6180477ce1b64d
A23FranciscoFC <a23franciscofc@a21eql09.sanclemente.local>
2024-02-06 13:40:39 +0100

    lex file estructura de nodo

--- lex.c
+++ lex.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+struct node{
+	int content;
+	struct node *right;
+	struct node *left;
+} root, child, childBis, childChild;
+
+struct node* lastchild(struct node *root){
+	int counter = 0;
+}
+
 int main() {
+	root.content = 9;
+	root.right = &child;
+	root.left = &childBis;
+	child.content = 8;
+	childBis.content = 7;
+	childBis.left = &childChild;
+	childChild.content = 6;
+
+	printf("%d\n", lastchild(&root)->content);
     return 0;
 }
